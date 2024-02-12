56fafe6be63059b15b5cbec64115193e997691e0
mahad002 <mahad112002@gmail.com>
2024-02-12 06:30:50 +0500

    added header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import './App.css'
+import Header from "./components/navbar/header";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <Router>
+          <Header/>
+          <Routes>
+            <Route index element={<h1>Hello</h1>} />
+            <Route path="movies/:type" element={<h1>Movie list for Type</h1>} />
+            <Route path="movie/:id" element={<h1>Movie Detail</h1>} />
+            <Route path="/*" element={<h1>Error 404 Page not available!</h1>} />
+          </Routes>
+        </Router>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
